cd732fdecbfecad38472063e6260af36cb19fa77
Zixon77 <chikaosro@gmail.com>
2025-01-12 20:46:44 -0500

    package updates

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,9 @@
-// Import the functions you need from the SDKs you need
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence, signInWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA4NBzvbD29-iWtFWX2UDRwKy_72pRjcW4",
   authDomain: "simply-xatalyst.firebaseapp.com",
